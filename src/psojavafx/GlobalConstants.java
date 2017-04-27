82101eb4e1ac6747cde3140384bea219bfc5dfd7
Ashwin Thyagarajan <ashwin031192@gmail.com>
2017-04-27 15:56:30 +0530

    Added vehicle transition calculations

---
+++ src/psojavafx/GlobalConstants.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package psojavafx;
+
+/**
+ * Global constants to be used throughout the application
+ * @author ashwin
+ */
+public class GlobalConstants {
+    
+    /**
+     * Represents the canvas' width
+     */
+    public static int canvasWidth = 1200;
+    
+    /**
+     * Represents the canvas' height
+     */
+    public static int canvasHeight = 700;
+    
+    public static int assignedLanes = 1;
+    
+    public static int scl = 10;
+    
+    public static int maxVehicles = 50;
+    
+    public static int nSecondRule = 2;
+}
